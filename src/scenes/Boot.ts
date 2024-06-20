9126826a6338e9aee852fac41b451b1d8b311161
Jacob Pow <bldev13453@hotmail.com>
2024-06-20 21:44:40 +0300

    feat: move to typescript

---
+++ src/scenes/Boot.ts
@@ -0,0 +1,25 @@
+import { Scene } from "phaser";
+
+export class Boot extends Scene {
+  constructor() {
+    super("Boot");
+  }
+
+  preload() {
+    this.load.image("background", "assets/bg.png");
+    this.load.image("player", "assets/player.png");
+    this.load.image("platform", "assets/platform.png");
+    this.load.image("heart", "assets/heart.png");
+    this.load.image("spike", "assets/spike.png");
+    this.load.image("coin", "assets/coin.png");
+    this.load.audio("coin", "audio/coin.mp3");
+    this.load.spritesheet("character_sprite", "assets/character_sprite.png", {
+      frameWidth: 256,
+      frameHeight: 256,
+    });
+  }
+
+  create() {
+    this.scene.start("Preloader");
+  }
+}
---
+++ src/scenes/MainMenu.ts
@@ -0,0 +1,39 @@
+import { Scene } from "phaser";
+
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
+
+  create() {
+    const { width, height } = this.scale;
+    const centerX = width / 2;
+    const centerY = height / 2;
+
+    this.add
+      .image(centerX, centerY, "background")
+      .setDisplaySize(width, height)
+      .setOrigin(0.5, 0.5);
+
+    const text = this.add
+      .text(width / 2, height / 2, "Menu", {
+        fontFamily: "Arial Black",
+        fontSize: Math.round(height * 0.05),
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: Math.round(height * 0.01),
+        align: "center",
+      })
+      .setOrigin(0.5);
+
+    // click text
+    text.setInteractive();
+    text.on("pointerdown", () => {
+      this.scene.start("Game");
+    });
+
+    // this.input.once('pointerdown', () => {
+    //     this.scene.start('Game');
+    // });
+  }
+}
